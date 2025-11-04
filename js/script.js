d1b258b129d14d1bb3938ef49c6c93aa84cb19c6
oratilemdladla <231263@virtualwindow.co.za>
2025-11-04 19:58:18 +0200

    nav bar js
    nav bar js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if(scroll > 100){
-            $(".takeTwo").css("background", "#0C0C0C")
-        }
-        else{
-            $(".netflix-navbar").css("background", "transparent")
-        }
-    })
-})
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if(scroll > 100){
+//             $(".takeTwo").css("background", "#0C0C0C")
+//         }
+//         else{
+//             $(".netflix-navbar").css("background", "transparent")
+//         }
+//     })
+// })
+
 
