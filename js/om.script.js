010af72c7a3dd5f9849b5fae6fe342a897d1eca0
oratilemdladla <231263@virtualwindow.co.za>
2025-11-05 09:20:29 +0200

    update in watchlist html
    update in watchlist html

--- js/om.script.js
+++ js/om.script.js
@@ -10,4 +10,23 @@
 //     })
 // })
 
+!async function(){
+const url = 'https://imdb236.p.rapidapi.com/api/imdb/cast/nm0000190/titles';
+const options = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'ecdd572f6fmsh055b23482742d2cp1af123jsn9b1d66941f6f',
+        'x-rapidapi-host': 'imdb236.p.rapidapi.com'
+    }
+};
+
+    let data = await fetch(url, options)
+                .then((response) => response.json())
+                .then((result)=> {return result})
+                .then ((error) => console.log(error));
+
+    console.log(data);
+
+}();
+
 
